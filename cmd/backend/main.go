7ce36e04e92b19adcb83c8ea6edf973ee2787fd2
Chris Hartley <chartleyit@gmail.com>
2020-08-30 11:27:53 +0000

    add new function and deploy to cloud functions

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"html"
+	"net/http"
+)
 
 func main() {
 	fmt.Println(start())
@@ -8,4 +13,20 @@ func main() {
 
 func start() string {
 	return "Go Whiskey API backend"
+}
+
+func HelloHTTP(w http.ResponseWriter, r *http.Request) {
+	var d struct {
+		Message string `json: "message"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
+		fmt.Fprint(w, "Hello HTTP!")
+		return
+	}
+	if d.Message == "" {
+		fmt.Fprint(w, "Hello HTTP!")
+		return
+	}
+	fmt.Fprint(w, html.EscapeString(d.Message))
 }
