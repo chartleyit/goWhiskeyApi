363c6214dd0e5b5ff046d3623a32db9a553b641c
Chris Hartley <chartleyit@gmail.com>
2020-08-30 11:28:26 +0000

    add additional tests

--- cmd/backend/main_test.go
+++ cmd/backend/main_test.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
 var startString = "Go Whiskey API backend"
 
-func TestMain(t *testing.T) {
+func TestStart(t *testing.T) {
 	if s := start(); s != startString {
 		t.Error("got", start(), "expected", startString)
 	}
+}
+
+func TestMain(m *testing.M) {
+	eVal := m.Run()
+	os.Exit(eVal)
 }
